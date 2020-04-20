de75cc26e90c5956b9ccef2bd36aca8b5288a68f
lalaalal <bontin0128@gmail.com>
2020-04-20 21:50:20 +0900

    Allow all hosts

---
+++ ImperialBreath.Website/wwwroot/js/tasks.js
@@ -0,0 +1,55 @@
+﻿function toggleTaskDone(index, taskId) {
+    if ($('#tasks .custom-control-input')[index].checked) {
+        $('#tasks .card')[index].classList.add('checked');
+        $('#tasks .card-title')[index].classList.add('checked');
+        $('#tasks .card-body')[index].classList.add('collapsable');
+        $.get("/users/toggle-task?taskId=" + taskId);
+    } else {
+        $('#tasks .card')[index].classList.remove('checked');
+        $('#tasks .card-title')[index].classList.remove('checked');
+        $('#tasks .card-body')[index].classList.remove('collapsable');
+        $.get("/users/toggle-task?taskId=" + taskId);
+    }
+}
+
+function changeShow() {
+    var select = $('#select')[0];
+    $('#tasks .container').show();
+    if (select.value == "all") {
+        $('#tasks .card').show();
+    } else if (select.value == "done") {
+        $('#tasks .card').hide();
+        $('#tasks .card.checked').show();
+        hideTaskContainer();
+    } else if (select.value == "undone") {
+        $('#tasks .card').show();
+        $('#tasks .card.checked').hide();
+        hideTaskContainer();
+    }
+}
+
+function isTaskContainerEmpty(index) {
+    var children = $('#tasks .container')[index].children;
+    for (var i = 1; i < children.length; i++) {
+        if (children[i].style.display != "none")
+            return false;
+    }
+    return true;
+}
+
+function hideTaskContainer() {
+    for (var i = 0; i < $('#tasks .container').length; i++) {
+        if (isTaskContainerEmpty(i))
+            $('#tasks .container')[i].style.display = "none";
+    }
+}
+
+for (var index = 0; index < $('#tasks .custom-control-input').length; index++) {
+    if ($('#tasks .custom-control-input')[index].checked) {
+        $('#tasks .card')[index].classList.add('checked');
+        $('#tasks .card-title')[index].classList.add('checked');
+        $('#tasks .card-body')[index].classList.add('collapsable');
+    }
+}
+
+changeShow($('#select')[0]);
