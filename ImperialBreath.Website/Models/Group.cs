4bd5b9fa72c224a4a727c8b6c75997b6d5b7d645
lalaalal <bontin0128@gmail.com>
2020-04-26 23:03:20 +0900

    Edit Title

---
+++ ImperialBreath.Website/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ImperialBreath.Website.Models
+{
+    public class Group
+    {
+        [JsonPropertyName("subjects")]
+        public Subject[] Subjects { get; set; }
+
+        [JsonPropertyName("tasks")]
+        public Models.Task[] Tasks { get; set; }
+    }
+}
